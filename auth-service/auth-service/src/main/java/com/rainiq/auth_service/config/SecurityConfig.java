58d098ef76cdd84e9e058644dcc175d7bb793bed
Skagar <gsamaksha@gmail.com>
2026-06-12 18:23:43 +0530

    Updated Required files

--- auth-service/auth-service/src/main/java/com/rainiq/auth_service/config/SecurityConfig.java
+++ auth-service/auth-service/src/main/java/com/rainiq/auth_service/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.rainiq.auth_service.config;
 
+import com.rainiq.auth_service.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
@@ -23,9 +30,13 @@ public class SecurityConfig {
                         "/swagger-ui/**",
                         "/api-docs/**"
                 ).permitAll().anyRequest().authenticated()).sessionManagement(
-                        session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) //STATELESS=>Never create a session. Never store authentication in memory. Every request must carry its own proof of identity — a JWT token
-        );
+                        session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)//STATELESS=>Never create a session. Never store authentication in memory. Every request must carry its own proof of identity — a JWT token
+        ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
     @Bean
     public PasswordEncoder passwordEncoder()
