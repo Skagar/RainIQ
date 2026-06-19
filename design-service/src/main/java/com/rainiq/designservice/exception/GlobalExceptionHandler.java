68a25ae83d3752564017fc54f5c8c6845fe6d3f5
Skagar <gsamaksha@gmail.com>
2026-06-19 22:51:10 +0530

    Created Custom exception for handling validation errors

--- design-service/src/main/java/com/rainiq/designservice/exception/GlobalExceptionHandler.java
+++ design-service/src/main/java/com/rainiq/designservice/exception/GlobalExceptionHandler.java
@@ -2,11 +2,13 @@ package com.rainiq.designservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -30,6 +32,21 @@ public class GlobalExceptionHandler {
                 exception.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(errorResponse,HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> validationException(MethodArgumentNotValidException exception, WebRequest request) {
+        String errorMessage = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        Response errorResponse = new Response(
+                new Date(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Failed",
+                errorMessage,
+                request.getDescription(false));
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
