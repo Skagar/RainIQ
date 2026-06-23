fe5bc0417b2e863792180062d9750445de893826
Skagar <gsamaksha@gmail.com>
2026-06-23 19:40:31 +0530

    Created Basic setup of Rainfall  service

---
+++ rainfall-service/src/main/java/com/rainiq/rainfallservice/RainfallServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rainiq.rainfallservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainfallServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainfallServiceApplication.class, args);
+    }
+
+}
---
+++ rainfall-service/src/test/java/com/rainiq/rainfallservice/RainfallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainiq.rainfallservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainfallServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
