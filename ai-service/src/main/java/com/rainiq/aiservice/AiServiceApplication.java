c3f3fc0de4aa6044f3a324aeb3e9022649d214c9
Skagar <gsamaksha@gmail.com>
2026-07-21 14:34:28 +0530

    Resolved conflict  of timezone between JVM and Postgres

--- ai-service/src/main/java/com/rainiq/aiservice/AiServiceApplication.java
+++ ai-service/src/main/java/com/rainiq/aiservice/AiServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiServiceApplication {
 
     public static void main(String[] args) {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(AiServiceApplication.class, args);
     }
 
