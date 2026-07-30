94b85dac4d922b72cf2ac6084cd213500736c73e
Skagar <gsamaksha@gmail.com>
2026-07-30 23:43:32 +0530

    Add Kafka Docker, prometheus, and db  configurations

---
+++ kafka-docker/init-db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE rainiq_auth;
+CREATE DATABASE rainiq_design;
+CREATE DATABASE rainiq_property;
+CREATE DATABASE rainiq_rainfall;
+CREATE DATABASE rainiq_compliance;
+CREATE DATABASE rainiq_monitoring;
+CREATE DATABASE rainiq_ai;
+CREATE DATABASE rainiq_notification;
